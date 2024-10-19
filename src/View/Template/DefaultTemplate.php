38b7d40b55708716859be2831734373c068337d5
Sean Patrick <shawnjoyce84@gmail.com>
2024-10-19 09:41:55 -0600

    Routing Implemented with htaccess
    I got the routing and htaccess completed. Added photos and UI to defaulttemplate and home

--- src/View/Template/DefaultTemplate.php
+++ src/View/Template/DefaultTemplate.php
@@ -7,8 +7,37 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Sean Patrick - Web Development - Western Slope, Colorado</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/default.css" rel="stylesheet">
+        <link rel="icon" type="image/png" href="/images/favicon.ico">
+        <meta name="description" content="Portfolio and micro-blog of Sean Patrick Joyce, a web developer on the Colorado Western Slope.">
+        <meta name="keywords" content="Web Development, Web Design, Application Development, PHP, HTML, CSS, Javascript, Laravel, React, Vue, Mysql, PDO, Godot, Game Development, Sean Patrick, Sean Patrick Joyce">
+        <meta name="robots" content="index,follow">
     </head>
     <body>
-        {{content}}
+
+    <div class="container-fluid p-0 m-0">
+
+         <div class="row banner-bg vh-100 m-0 p-0">
+         
+            <div class="col-lg-12 p-0 m-0">
+{{content}}
+            </div> 
+
+            <div class="clearfix mt-auto"> 
+                <img src="images/cat_banner.webp" class="img-fluid cat-banner z-3 position-absolute bottom-0 end-0" alt="SPJ Cat Icon">
+            </div>
+
+            <div class="col-lg-12 d-flex align-items-end p-0 m-0">
+                <img src="images/back_dust.webp" class="img-fluid back-dust z-0 position-absolute" alt="SPJ Background Dust">
+                <img src="images/face_banner.webp" class="img-fluid face-banner z-1 position-absolute" alt="SPJ Banner">
+                <img src="images/front_dust.webp" class="img-fluid front-dust z-2 position-absolute" alt="SPJ Foreground Dust">
+            </div>
+            
+         </div>
+
+    </div>
+
+        <script src="js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
--- src/View/home.php
+++ src/View/home.php
@@ -1,1 +1,15 @@
-<a href="/about">boobs?</a
+                <div class="row">
+                    
+                    <div class="col-lg-4">
+                        <h1 class="banner-text text-uppercase text-end">Sean<br />Patrick<br />Joyce</h1>
+
+                        <nav class="text-end">
+                            <a class="nav-link port-link" href="#">Projects</a>
+                            <a class="nav-link port-link" href="#">About</a>
+                        </nav>
+                    </div>
+
+                    <div class="col-lg-8 my-4">
+                        oh no!
+                    </div>
+                </div>
